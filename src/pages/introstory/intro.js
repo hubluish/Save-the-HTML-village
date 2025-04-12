08e6f364ab043c24bd0fa226dac6748672891acf
gaeun2o <162069275+gaeun2o@users.noreply.github.com>
2025-04-12 15:37:29 +0900

    [feat]introstory 제작중
    endstory 코드 참고해서 초안만 잡아두었습니다

--- src/pages/introstory/intro.js
+++ src/pages/introstory/intro.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const totalSteps = 4;
+  const totalSteps = 9;
   const stepper = document.querySelector('.stepper');
   let currentStep = 0;
 
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // next-btn 클릭 시 clear.html로 이동
   document.querySelector('.next-btn').addEventListener('click', function () {
     if (currentStep === totalSteps - 1) {
-      window.location.href = '../clear.html';
+      window.location.href = '../start.html';
     } else {
       currentStep++;
       updateSteps();
@@ -48,10 +48,15 @@ document.addEventListener('DOMContentLoaded', function () {
 // 텍스트 내용 반환 함수
 function getBottomText(stepIndex) {
   switch (stepIndex) {
-    case 0: return '(평화로운 마을소리)';
-    case 1: return "덕분에 평화로운 우리 마을을 되찾았어!";
-    case 2: return "넌 분명 좋은 개발자가 될거야!<br>응원할게 >~<"  ;
-    case 3: return "다음에는 행복한 얼굴로 만나뭉 안뇨~~";
+    case 0: return '우르르 쾅쾅.. 주르륵..';
+    case 1: return "마을 주민들: 으아아!! <br>우리 마을 어떡해ㅠㅠㅠ";
+    case 2: return "으악 저기 좀 봐!<br>시장이 사라졌어..."  ;
+    case 3: return "으앗!!<br>마을이 모두 사라지고 있잖아?!?"; 
+    case 4: return "마을이 흔적도 없이 사라졌어..<br>어떡하지.."; 
+    case 5: return "우리 마을은 HTML이랑 CSS로만<br>복구할 수 있는데 .. 어휴"; 
+    case 6: return "어랏??<br>키보드 타이핑 소리가 들려";
+    case 7: return "우리 마을을 구해줄 사람이 있는 거 같아! <br>저쪽으로 가보자 ";
+    case 8: return "앗 눈부셔!.. 당신이군요!<br>우리 마을을 구해줄사람이 !!!";
     default: return "";
   }
 }
@@ -62,21 +67,20 @@ function getMiddleContent(stepIndex) {
     case 0:
       return `
         <div class="image-container">
-          <img src="../../assets/icons/story_end_1.png" class="bg-image" />
+          <img src="../../assets/icons/story_intro_1.png" class="bg-image" />
         </div>
       `;
     case 1:
       return `
         <div class="image-container">
-          <img src="../../assets/icons/story_end_2.png" class="bg-image" />
-          <img src="../../assets/icons/story_end_2-2.png" class="overlay-image" />
+          <img src="../../assets/icons/story_intro_2.png" class="bg-image" />
         </div>
       `;
     case 2:
       return `
         <div class="image-container">
-          <img src="../../assets/icons/story_end_2.png" class="bg-image" />
-          <img src="../../assets/icons/story_end_3.png" class="overlay-image" />
+          <img src="../../assets/icons/story_intro_2.png" class="bg-image" />
+          <img src="../../assets/icons/story_intro_3.png" class="overlay-image" />
         </div>
       `;
     case 3:
