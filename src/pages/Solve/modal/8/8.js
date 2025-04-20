f7d2be20c746508bb0fe2ada346cf175ee577ce6
gaeun2o <162069275+gaeun2o@users.noreply.github.com>
2025-04-20 16:46:31 +0900

    [Feat] 8번 문제 추가
    9번과 함께 문제에서 확인해볼 예정입니다.

--- src/pages/Solve/modal/8/8.js
+++ src/pages/Solve/modal/8/8.js
@@ -1,3 +1,24 @@
-document.querySelector(".next-stage-button").addEventListener("click", () => {
-    window.location.href = "../9/9.html"; // 다음 스테이지로 이동
-});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const screen1 = document.getElementById('screen1');
+    const screen2 = document.getElementById('screen2');
+    const checkBtn = document.querySelector('.check-button');
+    const nextStageBtn = document.querySelector('.next-stage-button');
+  
+    // 5초 뒤 두 번째 화면 보여주기
+    setTimeout(() => {
+        screen1.classList.add('hidden');
+        screen2.classList.remove('hidden');
+    }, 5000);
+  
+    // Click! 버튼 클릭 시 세 번째 화면으로 이동
+    checkBtn.addEventListener('click', () => {
+        screen2.classList.add('hidden');
+        screen3.classList.remove('hidden');
+    });
+  
+    document.querySelector(".next-stage-button").addEventListener("click", () => {
+        window.location.href = "../9/9.html"; // 다음 스테이지로 이동
+    });
+  });
+  
--- src/pages/Solve/modal/9/9.js
+++ src/pages/Solve/modal/9/9.js
@@ -2,7 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const screen1 = document.getElementById('screen1');
   const screen2 = document.getElementById('screen2');
   const screen3 = document.getElementById('screen3');
-  const checkBtn = document.querySelector('.check-button');
   const nextStageBtn = document.querySelector('.next-stage-button');
 
   // 5초 뒤 두 번째 화면 보여주기
@@ -10,12 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
       screen1.classList.add('hidden');
       screen2.classList.remove('hidden');
   }, 5000);
-
-  // Click! 버튼 클릭 시 세 번째 화면으로 이동
-  checkBtn.addEventListener('click', () => {
-      screen2.classList.add('hidden');
-      screen3.classList.remove('hidden');
-  });
 
   // 확인! 버튼 클릭 시 다음 페이지로 이동
   nextStageBtn.addEventListener('click', () => {
